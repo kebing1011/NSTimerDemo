eabdfa623a5e50901c2fc04e1df5f6da635987c2
maokebing <kebing1011@163.com>
2015-07-20 09:43:39 +0800

    first commit

---
+++ NSTimerDemo/TTObject.h
@@ -0,0 +1,16 @@
+//
+//  TTObject.h
+//  NSTimerDemo
+//
+//  Created by mao on 7/17/15.
+//  Copyright (c) 2015 mao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TTObject : NSObject
+
+- (void)startTimer;
+- (void)stopTimer;
+
+@end
---
+++ NSTimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSTimerDemo
+//
+//  Created by mao on 7/17/15.
+//  Copyright (c) 2015 mao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
